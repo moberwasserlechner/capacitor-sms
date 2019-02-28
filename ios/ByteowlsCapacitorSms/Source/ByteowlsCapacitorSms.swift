e82351a0807be0eb9fd5f32dca5ed870e55cbba7
Michael Oberwasserlechner <michaelowl@gmail.com>
2019-02-28 21:08:03 +0100

    feat(ios): #1 send sms by opening message app

--- ios/ByteowlsCapacitorSms/Source/ByteowlsCapacitorSms.swift
+++ ios/ByteowlsCapacitorSms/Source/ByteowlsCapacitorSms.swift
@@ -1,67 +1,57 @@
 import Foundation
 import Capacitor
+import MessageUI
 
 typealias JSObject = [String:Any]
 
 @objc(SmsManagerPlugin)
-public class SmsManagerPlugin: CAPPlugin {
-
+public class SmsManagerPlugin: CAPPlugin, MFMessageComposeViewControllerDelegate {
+    
     let PARAM_NUMBERS = "numbers"
     let PARAM_TEXT = "text"
-
+    
+    var pluginCall: CAPPluginCall?
+    
+    public func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+       
+        switch (result.rawValue) {
+        case MessageComposeResult.cancelled.rawValue:
+            self.pluginCall!.reject("SEND_CANCELLED")
+        case MessageComposeResult.failed.rawValue:
+            self.pluginCall!.reject("ERR_SEND_FAILED")
+        case MessageComposeResult.sent.rawValue:
+            self.pluginCall!.success()
+        default:
+            self.pluginCall!.reject("ERR_SEND_UNKOWN_STATE")
+        }
+        controller.dismiss(animated: true, completion: nil)
+    }
 
     @objc func send(_ call: CAPPluginCall) {
-        guard let numbers = getString(call, PARAM_NUMBERS) else {
-            call.reject("ERR_INVALID_SMS")
+        self.pluginCall = call
+        guard let numbers = call.getArray(PARAM_NUMBERS, String.self) else {
+            call.reject("ERR_NO_NUMBERS")
             return
         }
-        guard let text = getString(call, PARAM_TEXT) else {
-            call.reject("ERR_INVALID_SMS")
+        guard let text = call.getString(PARAM_TEXT) else {
+            call.reject("ERR_NO_TEXT")
             return
         }
-    }
-
-    private func getConfigObjectDeepest(_ options: [AnyHashable: Any?]!, key: String) -> [AnyHashable:Any?]? {
-        let parts = key.split(separator: ".")
-
-        var o = options
-        for (_, k) in parts[0..<parts.count-1].enumerated() {
-            if (o != nil) {
-                o = o?[String(k)] as? [String:Any?] ?? nil
-            }
+        
+        if !MFMessageComposeViewController.canSendText() {
+            call.reject("ERR_SMS_SERVICE_NOTFOUND")
+            return
         }
-        return o
-    }
-
-    private func getConfigKey(_ key: String) -> String {
-        let parts = key.split(separator: ".")
-        if parts.last != nil {
-            return String(parts.last!)
+        
+        let composeVC = MFMessageComposeViewController()
+        composeVC.messageComposeDelegate = self
+        // Configure the fields of the interface.
+        composeVC.recipients = numbers
+        composeVC.body = text
+        // Present the view controller modally.
+        DispatchQueue.main.async {
+            // Update UI
+            self.bridge.viewController.present(composeVC, animated: true, completion: nil);
         }
-        return ""
     }
-    
-    private func getOverwritableString(_ call: CAPPluginCall, _ key: String) -> String? {
-        var base = getString(call, key)
-        let ios = getString(call, "ios." + key)
-        if ios != nil {
-            base = ios
-        }
-        return base;
-    }
-
-    private func getValue(_ call: CAPPluginCall, _ key: String) -> Any? {
-        let k = getConfigKey(key)
-        let o = getConfigObjectDeepest(call.options, key: key)
-        return o?[k] ?? nil
-    }
-
-    private func getString(_ call: CAPPluginCall, _ key: String) -> String? {
-        let value = getValue(call, key)
-        if value == nil {
-            return nil
-        }
-        return value as? String
-    }
-
 }
