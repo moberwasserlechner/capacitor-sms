6c631c97b2b94495379f0515a78e15e5863bb712
jcesarmobile <jcesarmobile@gmail.com>
2023-09-01 20:16:28 +0200

    feat!: Update to Capacitor 5 (#21)

--- src/web.ts
+++ src/web.ts
@@ -3,19 +3,8 @@ import {SmsManagerPlugin, SmsSendOptions} from "./definitions";
 
 export class SmsManagerPluginWeb extends WebPlugin implements SmsManagerPlugin {
 
-    NOT_SUPPORTED: string = "ERR_PLATFORM_NOT_SUPPORTED";
-
-    constructor() {
-        super({
-            name: 'SmsManager',
-            platforms: ['web']
-        });
-    }
-
-    async send(options: SmsSendOptions): Promise<void> {
-        return new Promise<void>((resolve, reject) => {
-            reject(this.NOT_SUPPORTED);
-        });
+    async send(): Promise<void> {
+        throw this.unimplemented('Not implemented on web.');
     }
 
 }
